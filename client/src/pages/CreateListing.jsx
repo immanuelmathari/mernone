faa2aacf07942c9b5870f8da6aa9fc0c10aa190d
Evil Genius <immanuelnganga2001@gmail.com>
2024-05-21 12:14:56 +0300

    upload listing images Functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,8 +1,75 @@
 // This is a private route
 
-import React from 'react'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import React, { useState } from 'react'
+import { app } from '../firebase';
 
 export default function CreateListing() {
+    const [files, setFiles] = useState([]);
+    // console.log(files)
+    const [formData, setFormData] = useState({
+        imageUrls: [],
+    });
+    console.log(formData.imageUrls.length);
+    const [imageUploadError, setImageUploadError] = useState(false);
+    // for loading
+    const [uploading, setUploading] = useState(false);
+    const handleImageSumbit = (e) => {
+        // e.preventDefault();  bcz we aint in the form
+        if(files.length > 0 && files.length  < 7){
+            setUploading(true);
+            setImageUploadError(false);
+            const promises = []; // we'll have more than one asynchronous behaious because we have many files submitting
+
+            for (let i=0 ; i<files.length ; i++){
+                promises.push(storeImage(files[i])); // our downloaded urls will be stored in this promise
+            }
+            // we wait for the promises to come to the promise as we store the urls
+            Promise.all(promises).then((urls) => {
+                setFormData({...formData, imageUrls: formData.imageUrls.concat(urls)}); // we keep them in the form by adding new ones to the existing
+                setImageUploadError(false);
+                setUploading(false);
+            }).catch((error) => {
+                setImageUploadError('Image upload failed');
+                setUploading(false); 
+            });
+        } else {
+            setImageUploadError('You can only upload 6 images per listing');
+            setUploading(false);
+
+        }
+    };
+
+    const storeImage = async (file) => {
+        return new Promise((resolve,reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage,fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done`);
+                },
+                (error) => {
+                    reject(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                    });
+                }
+            );
+        });
+    }
+
+    const handleRemoveImage = (index) => {
+        setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+        })
+    } 
   return (
     // Main makes it seo friendly
     <main className='p-3 max-w-4xl mx-auto'>
@@ -68,11 +135,27 @@ export default function CreateListing() {
                     <span className="font-normal text-gray-600 ml-2">The first image will be the cover (max:6)</span>
                 </p>
                 <div className='flex gap-4'>
-                    <input type='file' id='images' accept="image/*" multiple className='p-3 border border-gray-300 rounded w-full' />
-                    <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                    <input type='file' id='images' accept="image/*" multiple className='p-3 border border-gray-300 rounded w-full' onChange={(e) => setFiles(e.target.files)} />
+                    <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80' onClick={handleImageSumbit}  disabled={uploading} >
+                        {uploading ? 'Uploading...' : 'Upload'}
+                    </button>
                 </div>
+                <p className='text-red-700 text-sm'>{imageUploadError && imageUploadError}</p>
+                {
+                    formData.imageUrls.length > 0 && formData.imageUrls.map((url,index) => (
+                        <div className='flex justify-between p-3 border items-center' key={url}> 
+                            <img src={url} alt='listing image' className='w-40 h-40 object-contain rounded-lg'/>
+                            {/* the function below should be a call back function else it will always be calling itself even without clicking */}
+                            <button type='button' className='p-3 text-red-700 rounded-lg uppercase hover:opacity-95' onClick={() => {handleRemoveImage(index)}}>Delete</button>
+                             </div>
+                        
+                    ))
+                }
+                
                 <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
 
+
+                
             </div>
         </form>
     </main>
