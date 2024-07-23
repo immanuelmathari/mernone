ce29789c06820fd857d608bd715103eb53b07a95
Evil Genius <immanuelnganga2001@gmail.com>
2024-07-23 10:45:27 +0300

    occupy jkia

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
@@ -10,6 +9,8 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import SignIn from './pages/SignIn';
+
 
 export default function App() {
   return (
